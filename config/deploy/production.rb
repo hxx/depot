61022f9405c05a9ca5d697e99a2de33d94487706
yellowstar <845059331@qq.com>
2014-06-01 09:59:44 +0800

    update the deploy VPS from Linode to DO

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@106.186.121.41}
-role :web, %w{deploy@106.186.121.41}
-role :db,  %w{deploy@106.186.121.41}
+role :app, %w{deploy@198.199.107.51}
+role :web, %w{deploy@198.199.107.51}
+role :db,  %w{deploy@198.199.107.51}
 
 # Extended Server Syntax
 # ======================
@@ -14,7 +14,7 @@ role :db,  %w{deploy@106.186.121.41}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '106.186.121.41', user: 'deploy', roles: %w{web}, my_property: :my_value
+server '198.199.107.51', user: 'deploy', roles: %w{web}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
